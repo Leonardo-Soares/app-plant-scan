48d848aca9c7fb12f458bb9fcc75ea8525c4b663
Leonardo-Soares <leonardoafonso1048@gmail.com>
2023-11-09 22:33:07 -0300

    chore: navegação menu

--- src/components/CardTopNavigation.tsx
+++ src/components/CardTopNavigation.tsx
@@ -1,11 +1,33 @@
 import React, { useState } from 'react'
 import { colors } from '@theme/colors'
-import { Modal, Text, Image, TouchableOpacity, View } from 'react-native'
 import IcoMenu from '../../assets/svg/IcoMenu'
 import IcoClose from '../../assets/svg/IcoClose'
+import { useNavigate } from '@hooks/useNavigate'
+import useAuthenticatedStore from '@stores/useAuthenticatedStore'
+import { Modal, Text, Image, TouchableOpacity, View } from 'react-native'
 
 export default function CardTopNavigation() {
+    const { navigate } = useNavigate()
     const [modalMenu, setModalMenu] = useState(false)
+    const { setIsAuthenticated } = useAuthenticatedStore()
+
+    function onHome() {
+        navigate('HomeScreen')
+        setModalMenu(false)
+    }
+    function onCadastros() {
+        navigate('')
+        setModalMenu(false)
+    }
+    function onCarteira() {
+        navigate('')
+        setModalMenu(false)
+    }
+    function onLogout() {
+        setIsAuthenticated(false)
+        navigate('LoginScreen')
+        setModalMenu(false)
+    }
 
     return (
         <>
@@ -15,10 +37,18 @@ export default function CardTopNavigation() {
                         <IcoClose />
                     </TouchableOpacity>
                     <View className='items-end gap-y-8'>
-                        <Text className='text-4xl font-bold text-white'>Home</Text>
-                        <Text className='text-4xl font-bold text-white'>Cadastros</Text>
-                        <Text className='text-4xl font-bold text-white'>Carteirinha</Text>
-                        <Text className='text-4xl font-bold text-white'>Sair</Text>
+                        <TouchableOpacity className='z-50' onPress={onHome}>
+                            <Text className='text-4xl font-bold text-white'>Home</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity className='z-50' onPress={() => navigate('')}>
+                            <Text className='text-4xl font-bold text-white'>Cadastros</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity className='z-50' onPress={() => navigate('')}>
+                            <Text className='text-4xl font-bold text-white'>Carteirinha</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity className='z-50' onPress={onLogout}>
+                            <Text className='text-4xl font-bold text-white'>Sair</Text>
+                        </TouchableOpacity>
                     </View>
                     <Image className='absolute h-full flex-1 opacity-40' source={require('../../assets/img/bg-fundo-menu.png')} />
                 </View>
